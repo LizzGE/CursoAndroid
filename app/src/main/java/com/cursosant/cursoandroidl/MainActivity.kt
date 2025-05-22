d3f80bb6479b7cb50dbf663755359b86fce37b41
Lizzet Garcia Estrada <lizzetgarcia180@gmail.com>
2025-05-22 17:18:46 -0600

    Practica: Pasar datos entre fragmentos

--- app/src/main/java/com/cursosant/cursoandroidl/MainActivity.kt
+++ app/src/main/java/com/cursosant/cursoandroidl/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.cursosant.cursoandroidl
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
