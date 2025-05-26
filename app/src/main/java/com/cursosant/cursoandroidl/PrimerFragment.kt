5f11b902852842528cc3c472f86717ca3f28c55c
Lizzet Garcia Estrada <lizzetgarcia180@gmail.com>
2025-05-26 15:33:31 -0600

    Practica: Configuracion de view binding

--- app/src/main/java/com/cursosant/cursoandroidl/PrimerFragment.kt
+++ app/src/main/java/com/cursosant/cursoandroidl/PrimerFragment.kt
@@ -1,17 +1,31 @@
 package com.cursosant.cursoandroidl
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.commit
+import com.cursosant.cursoandroidl.databinding.FragmentPrimerBinding
 
 class PrimerFragment : Fragment(R.layout.fragment_primer) {
+
+    private lateinit var binding: FragmentPrimerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPrimerBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.btn_navegar)
+        val button = binding.btnNavegar
         button.setOnClickListener {
             requireActivity().supportFragmentManager.commit {
                 replace(R.id.fragment_container_view, SegundoFragment.newInstance("Lizz", 22))
--- app/src/main/java/com/cursosant/cursoandroidl/SegundoFragment.kt
+++ app/src/main/java/com/cursosant/cursoandroidl/SegundoFragment.kt
@@ -1,17 +1,30 @@
 package com.cursosant.cursoandroidl
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.setFragmentResult
+import com.cursosant.cursoandroidl.databinding.FragmentSegundoBinding
 
 class SegundoFragment : Fragment(R.layout.fragment_segundo) {
 
+    private lateinit var binding: FragmentSegundoBinding
     private var nombre: String? = ""
     private var edad: Int? = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSegundoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +36,8 @@ class SegundoFragment : Fragment(R.layout.fragment_segundo) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val text = view.findViewById<TextView>(R.id.txt_output)
-        val button = view.findViewById<Button>(R.id.btn_enviar_datos)
+        val text = binding.txtOutput
+        val button = binding.btnEnviarDatos
 
         button.setOnClickListener {
             val result = "Resultado"
